8e22d874f87b22f5524e3781a44d47f1f561258f
Alan Vàsquez <avasquez@posfile.com>
2024-10-07 15:53:06 -0600

    1er commit

---
+++ public/generated/javascript/default/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'public',
+  location: 'us-central1'
+};
+
