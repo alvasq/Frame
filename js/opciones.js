afc47de358138635cf824cbcd93d382e55dfb936
Alan Vàsquez <avasquez@posfile.com>
2024-10-11 23:38:50 -0600

    prueba

--- js/opciones.js
+++ js/opciones.js
@@ -7,25 +7,25 @@ var Frame = angular.module('fApp', []);
 Frame.controller('fControler', function ($scope) {
     // Función para manejar la opción de ingresar un usuario
     $scope.ingresarUsuario = function () {
-        window.location.href="usuario.html"
+        window.location.href="usuario"
         // Lógica para ingresar usuario (puedes agregar más detalles aquí)
     };
 
     // Función para manejar la opción de ingresar un aporte
     $scope.ingresarAporte = function () {
-        window.location.href="aportes.html"
+        window.location.href="aportes"
         // Lógica para ingresar un aporte (puedes agregar más detalles aquí)
     };
 
     // Función para generar el reporte de usuarios
     $scope.reporteUsuario = function () {
-        window.location.href="repusuarios.html"
+        window.location.href="repusuarios"
         // Lógica para generar reporte de usuario (puedes agregar más detalles aquí)
     };
 
     // Función para generar el reporte de aportes
     $scope.reporteAportes = function () {
-        window.location.href="repAportes.html"
+        window.location.href="repAportes"
         // Lógica para generar reporte de aportes (puedes agregar más detalles aquí)
     };
 });
